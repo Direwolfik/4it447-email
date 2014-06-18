bab7ddaaa86a6f5c6d1d97c3b1f0e3079e67aeb4
Boosik <Boosik@github.com>
2014-06-18 18:54:19 +0200

    test

---
+++ simpleWebApp-archetype/src/test/java/ContactsTest.java
@@ -0,0 +1,18 @@
+import junit.framework.TestCase;
+
+
+public class ContactsTest extends TestCase {
+
+	public ContactsTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
